7c2ea8252f5274e664e0275e43395807f49e3558
Sabbir <56760478+kmsabbir@users.noreply.github.com>
2021-02-26 00:03:57 +0600

    Update IncreasingArray.java

--- java/IncreasingArray.java
+++ java/IncreasingArray.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class IncreasingArray {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		int n;
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
